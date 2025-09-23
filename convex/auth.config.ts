1f45e60022380fb3c9333323a4c414e9a46e2f24
Nikoloz Turazashvili <turazashvili@gmail.com>
2025-09-23 15:44:26 +0700

    transitioning to workos auth JWT template

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,9 +4,9 @@ export default {
   providers: [
     {
       type: "customJwt",
-      issuer: `https://apiauth.convex.dev/user_management/${clientId}`,
+      issuer: `https://api.workos.com/user_management/${clientId}`,
       algorithm: "RS256",
-      jwks: `https://apiauth.convex.dev/sso/jwks/${clientId}`,
+      jwks: `https://api.workos.com/sso/jwks/${clientId}`,
       applicationID: clientId,
     },
   ],
