a2659cb095fd902b37666d94ba6f2460230daf26
Nikoloz Turazashvili <turazashvili@gmail.com>
2025-09-23 19:05:58 +0700

    brought back applicationID

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -6,7 +6,8 @@ export default {
       type: "customJwt",
       issuer: `https://api.workos.com/user_management/${clientId}`,
       algorithm: "RS256",
-      jwks: `https://api.workos.com/sso/jwks/${clientId}`
+      jwks: `https://api.workos.com/sso/jwks/${clientId}`,
+      applicationID: clientId,
     },
   ],
 };
