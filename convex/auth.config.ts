a467ce2a1d0133de2304a3527a848d55e9c5a44e
Nikoloz Turazashvili <turazashvili@gmail.com>
2025-09-23 19:26:03 +0700

    try apiauth

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,9 +4,9 @@ export default {
   providers: [
     {
       type: "customJwt",
-      issuer: `https://api.workos.com/user_management/${clientId}`,
+      issuer: `https://apiauth.convex.dev/user_management/${clientId}`,
       algorithm: "RS256",
-      jwks: `https://api.workos.com/sso/jwks/${clientId}`,
+      jwks: `https://apiauth.convex.dev/sso/jwks/${clientId}`,
       applicationID: clientId,
     },
   ],
